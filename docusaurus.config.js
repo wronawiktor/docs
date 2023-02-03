b3ee549d05135498c567584ba763af4680b0a58f
Michal Jura <mjura@suse.com>
2023-02-03 13:47:39 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'go4clouds',
   tagline: 'Clouds are cool!',
   url: 'https://go4clouds.github.io',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
